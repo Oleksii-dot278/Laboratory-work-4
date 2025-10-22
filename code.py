781ae24011f15a9c9cdd4c377ed74960311f62cb
Oleksii-dot278 <obukrieiev2023@itcollege.lviv.ua>
2025-10-22 19:28:40 +0200

    Updated

--- code.py
+++ code.py
@@ -1,1 +1,14 @@
+numbers = {1, 2, 3, 3, 2}
+print(numbers)  # {1, 2, 3} — 
 
+
+numbers.add(4)
+numbers.discard(2)
+print(numbers)
+
+
+a = {1, 2, 3}
+b = {3, 4, 5}
+print(a | b)  
+print(a & b)  
+print(a - b)  
